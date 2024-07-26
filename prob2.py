7d163ece282efb3539a1f31dd6e77c28a3a20184
Soham Jaiswal <sosweetham@gmail.com>
2024-07-26 21:15:44 +0530

    feat: prob3 sol

--- prob2.py
+++ prob2.py
@@ -1,7 +1,6 @@
 valLimit = int(input("Enter the maximum value: "))
 reqLength = int(input("Enter the maximum length: "))
 result = []
-
 def helper(current_array):
     if len(current_array) == reqLength:
         result.append(current_array)
@@ -9,8 +8,6 @@ def helper(current_array):
     last_element = current_array[-1]
     for i in range(last_element, valLimit+1, last_element):
         helper(current_array + [i])
-
 for i in range(1, valLimit+1):
     helper([i])
-
 print(len(result) % 10000)
