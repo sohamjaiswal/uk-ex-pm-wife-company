46c3e9269c9830dd6d75c0f339d5d8813095e07b
Soham Jaiswal <sosweetham@gmail.com>
2024-07-26 21:21:13 +0530

    feat: make concise

--- prob3.py
+++ prob3.py
@@ -1,7 +1,7 @@
 def getAscendingSubsequences(entries):
     subsequences, sequence = [], []
     for entry in entries:
-        if (not sequence) or (entry >= sequence[-1]):
+        if not sequence or entry >= sequence[-1]:
             sequence.append(entry)
         else:
             subsequences.append(sequence)
@@ -9,23 +9,9 @@ def getAscendingSubsequences(entries):
     subsequences.append(sequence)
     return subsequences
 def doesObeyLogic(sequence):
-    obeys = True
-    if len(sequence) == 1:
-        return False
-    for i in range(0, len(sequence)-1):
-        if not ((sequence[i] & sequence[i+1])*2<(sequence[i] | sequence[i+1])):
-            obeys = False
-            break
-    return obeys
+    return all((sequence[i] & sequence[i+1])*2 < (sequence[i] | sequence[i+1]) for i in range(len(sequence)-1))
 def getLongestWithLogic(subsequences):
-    longest = []
-    for sequence in subsequences:
-        if (len(sequence) > len(longest)) and doesObeyLogic(sequence):
-            longest = sequence
-    return longest
+    return max((sequence for sequence in subsequences if doesObeyLogic(sequence)), key=len, default=[])
 numEntries = int(input("Enter the number of entries: "))
-entries = []
-for i in range(0, numEntries):
-    entry = int(input("Enter entry " + str(i + 1) + ": "))
-    entries.append(entry)
+entries = [int(input(f"Enter entry {i + 1}: ")) for i in range(numEntries)]
 print(len(getLongestWithLogic(getAscendingSubsequences(entries))))
